1ec56352579c76a8687b60c548638467c2a808ab
Ayush Kumar <ayushk8295@gmail.com>
2025-07-31 22:20:15 +0530

    inprove some basic format in html and little style updation and add page reload button logic in js.

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ const accuracy = document.getElementById("accuracy");
 // console.log(paragraph);
 
 const arr = [
-  "The quick brown fox jumps over a lazy dog.",
-  "Pack my box with five dozen liquor jugs.",
-  "How vexingly quick daft zebras jump!",
-  "Sphinx of black quartz, judge my vow.",
-  "Jackdaws love my big sphinx of quartz.",
-  "Waltz, bad nymph, for quick jigs vex.",
-  "The five boxing wizards jump quickly.",
-  "Jived fox nymph grabs quick waltz.",
-  "Quick zephyrs blow, vexing daft Jim.",
-  "Two driven jocks help fax my big quiz.",
-  "Grumpy wizards make toxic brew for the jovial queen.",
-  "Big Fuji waves mock Zach the expert juggler.",
+  "The quick brown fox jumps over a lazy dog near the zigzagging riverbank, astonishing everyone with its grace and speed.",
+  "Pack my box with five dozen liquor jugs and gently place them next to the whimsical zebra painting hung above the fireplace.",
+  "How vexingly quick daft zebras jump across the lush valley while jolly kids dance to buzzing music in the plaza.",
+  "Sphinx of black quartz, judge my vow while the wizard brews a magical potion beside the flickering torchlight in the dim cave.",
+  "Jackdaws love my big sphinx of quartz that guards the enchanted temple surrounded by whispering pine trees and glowing fireflies.",
+  "Waltz, bad nymph, for quick jigs vex the stoic king who ponders quietly near the glimmering waterfall on a foggy night.",
+  "The five boxing wizards jump quickly through flaming hoops to amaze the royal guests seated under the starry sky in silence.",
+  "Jived fox nymph grabs quick waltz as a dazzling display of lightning arcs above the cliff near the frozen lake in winter.",
+  "Quick zephyrs blow, vexing daft Jim as he navigates the maze of neon signs flashing wildly across the midnight carnival square.",
+  "Two driven jocks help fax my big quiz answers to the quirky professor who sips herbal tea while balancing books on his head.",
+  "Grumpy wizards make toxic brew for the jovial queen while juggling flaming torches and taming a zebra wearing a velvet crown.",
+  "Big Fuji waves mock Zach the expert juggler who balances quartz spheres atop a bamboo stick on a windy mountaintop plateau.",
 ];
 
 const randInd = Math.round(Math.random() * 11);
@@ -35,10 +35,18 @@ let firstKeyPressed = false,
   index = 0,
   wrongChar = 0,
   endTime;
+let t = 0,
+  intervalId;
 document.addEventListener("keyup", function (event) {
   if (!firstKeyPressed) {
     start = performance.now();
+    // console.log(start);
     firstKeyPressed = true;
+    intervalId = setInterval(() => {
+      t++;
+      // console.log("ieie");
+      time.innerText = t;
+    }, 1000);
   }
 });
 
@@ -46,7 +54,6 @@ textArea.addEventListener("input", function (event) {
   // console.log(event.data);
   // console.log(paraArray);
   // console.log(index);
-
   if (paraArray[index] == event.data) {
     const result = paraArray
       .map((char, idx) => {
@@ -71,6 +78,8 @@ textArea.addEventListener("input", function (event) {
 
       const sp = Math.ceil((para.length * 60) / totalTime);
       speed.innerText = `${sp}CPM`;
+
+      clearInterval(intervalId);
     }
 
     paragraph.innerHTML = result;
@@ -79,7 +88,7 @@ textArea.addEventListener("input", function (event) {
     const result = paraArray
       .map((char, idx) => {
         if (index == idx) {
-          return `<span style="background-color:red;">${char}</span>`;
+          return `<span style="background-color:salmon;">${char}</span>`;
         } else {
           return `<span>${char}</span>`;
         }
@@ -88,4 +97,8 @@ textArea.addEventListener("input", function (event) {
 
     paragraph.innerHTML = result;
   }
+});
+
+btn.addEventListener("click", () => {
+  location.reload();
 });
